ee1f1b4b08fc391601e161c85880d061bd21752c
kaan <kaanatesel99@gmail.com>
2018-10-26 15:04:02 +0300

    First and Last commit

---
+++ src/mainPage.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        width: '400px',
+        height: '100%',
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
+    dense: {
+        marginTop: 16,
+    },
+    menu: {
+        width: 200,
+    },
+    button: {
+        margin: theme.spacing.unit,
+        marginLeft: '37%',
+    },
+    input: {
+        display: 'none',
+    },
+
+});
+
+const o2x = require('object-to-xml');
+
+
+
+class OutlinedTextFields extends React.Component {
+    state = {
+        name: 'Cat in the Hat',
+        age: '',
+        multiline: 'Controlled',
+        currency: 'EUR',
+        url: '',
+        xml: '',
+    };
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+
+    ToXml = () => {
+        const log = document.getElementById('log').value
+        const changefreq = document.getElementById('changefreq').value
+
+       
+        var obj = {
+            '?xml version="1.0" encoding="UTF-8"?': null,
+            urlset: {
+                '@': {
+                    xmlns:"https://www.sitemaps.org/schemas/sitemap/0.9",
+                },
+                '#': {
+                    url: {
+                        loc: log,
+                        changefreq: changefreq,
+                    }
+                }
+            }
+        };
+
+        this.setState({
+            xml: o2x(obj),
+        })
+
+
+        console.log(o2x(obj));
+
+    }
+
+
+    render() {
+
+        const { classes } = this.props;
+
+        return (
+            <Grid container className={classes.root} spacing={16}>
+                <Grid item xs={2}>
+
+                </Grid>
+                <Grid item xs={8}>
+                    <Grid container className={classes.root} spacing={16}>
+                        <Grid item xs={12}> <form className={classes.container} noValidate autoComplete="off">
+                            <TextField
+                                id="log"
+                                label="log"
+                                placeholder="log"
+                                multiline
+                                className={classes.textField}
+                                margin="normal"
+                                variant="outlined"
+                                value={this.state.inputValue}
+                                onChange={this.updateInputValue}
+                            />
+                            <TextField
+                                id="changefreq"
+                                label="changefreq"
+                                placeholder="changefreq"
+                                multiline
+                                className={classes.textField}
+                                margin="normal"
+                                variant="outlined"
+                                value={this.state.inputValue}
+                                onChange={this.updateInputValue}
+                            />
+                            <TextField
+                                id="xml"
+                                label="XML"
+                                placeholder=""
+                                multiline
+                                className={classes.textField}
+                                margin="normal"
+                                variant="outlined"
+                                value={this.state.xml}
+                                onChange={this.updateInputValue}
+                            />
+
+                        </form></Grid>
+                        <Grid item xs={12} >
+                            <Button variant="outlined" onClick={this.ToXml} color="primary" className={classes.button}>
+                                To XML
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                </Grid>
+                <Grid item xs={2}></Grid>
+
+            </Grid>
+
+        )
+    }
+
+}
+
+OutlinedTextFields.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(OutlinedTextFields);
